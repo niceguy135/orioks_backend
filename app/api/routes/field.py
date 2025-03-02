6455f0a96d041aadd9025395b1046d3755b1e0ad
Константин <danov@ancud.local>
2025-03-03 01:11:00 +0300

    add(routes/field.py): создал эндпоинт для получения ведомых групп по дисциплинам препода

--- app/api/routes/field.py
+++ app/api/routes/field.py
@@ -3,7 +3,9 @@ from typing import List
 
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
+from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
 
 import app.repository as repos
 from app.core.db import async_sessionmaker
@@ -17,7 +19,7 @@ class FieldComprehensionScheme(BaseModel):
     field_mark: int
 
 
-@router.get("/{student_id}", response_model=List[FieldComprehensionScheme])
+@router.get("/student/{student_id}", response_model=List[FieldComprehensionScheme])
 async def get_student_marks(student_id: int) -> List[FieldComprehensionScheme] | HTTPException:
     try:
         async with async_sessionmaker() as session:
@@ -41,6 +43,38 @@ async def get_student_marks(student_id: int) -> List[FieldComprehensionScheme] |
             )
 
         return response_data
+
+    except SQLAlchemyError as e:
+        return HTTPException(400, f"Bad DB request.\nCause:{e}")
+
+
+@router.get("/professor/{professor_id}/fields")
+async def get_professors_groups(professor_id: int) -> List[str] | HTTPException:
+
+    try:
+        session: AsyncSession
+        async with async_sessionmaker() as session:
+            query = await session.execute(
+                text(
+                    """
+                    with professor_fields as (
+                        select * from fields where professor_id = :professor_id
+                    ), educated_students as (
+                        select * from field_comprehensions where field in (select field_id from professor_fields)
+                    )
+                    select distinct students_group_number from students where student_id in (select student_id from educated_students)
+                    """
+                ),
+                {"professor_id": professor_id}
+            )
+
+            groups = []
+            for group_tuple in query:
+                groups.append(
+                    group_tuple[0]
+                )
+
+            return groups
 
     except SQLAlchemyError as e:
         return HTTPException(400, f"Bad DB request.\nCause:{e}")
