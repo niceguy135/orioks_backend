bdc8c50b9309e045b02ad689e52269ca52010142
Константин <danov@ancud.local>
2025-03-05 00:07:25 +0300

    add(routes/field.py): добавил (сделаных на коленке) эндпоинтов для получения журнала и оценок для препода

--- app/api/routes/field.py
+++ app/api/routes/field.py
@@ -7,8 +7,9 @@ from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 
-import app.repository as repos
+from app import repository as repos
 from app.core.db import async_sessionmaker
+from app.models import Fields
 
 router = APIRouter(prefix="/field")
 
@@ -19,13 +20,19 @@ class FieldComprehensionScheme(BaseModel):
     field_mark: int
 
 
+class StudentComprehension(BaseModel):
+    student_id: int
+    name: str
+    mark: int
+
+
 @router.get("/student/{student_id}", response_model=List[FieldComprehensionScheme])
 async def get_student_marks(student_id: int) -> List[FieldComprehensionScheme] | HTTPException:
     try:
         async with async_sessionmaker() as session:
             student_fields = await repos.FieldComprehensionRepository.get_by_filter(
                 session,
-                f"student_id == :student_id",
+                f"student_id = :student_id",
                 {"student_id": student_id}
             )
 
@@ -48,30 +55,78 @@ async def get_student_marks(student_id: int) -> List[FieldComprehensionScheme] |
         return HTTPException(400, f"Bad DB request.\nCause:{e}")
 
 
-@router.get("/professor/{professor_id}/fields")
-async def get_professors_groups(professor_id: int) -> List[str] | HTTPException:
+@router.get("/professor/{professor_id}/fields", response_model=List[Fields])
+async def get_professors_fields(professor_id: int) -> List[str] | HTTPException:
+    try:
+        session: AsyncSession
+        async with async_sessionmaker() as session:
+            fields = await repos.FieldRepository.get_by_filter(
+                session,
+                f"professor_id = :professor_id",
+                {"professor_id": professor_id}
+            )
 
+            return fields
+
+    except SQLAlchemyError as e:
+        return HTTPException(400, f"Bad DB request.\nCause:{e}")
+
+
+@router.get("/professor/{field_id}/groups", response_model=List[Fields])
+async def get_professors_field_groups(field_id: int) -> List[str] | HTTPException:
+    try:
+        session: AsyncSession
+        async with async_sessionmaker() as session:
+            res = await session.execute(
+                text(
+                    """
+                    with educated_students as (
+                        select * from field_comprehensions where field = :field_id
+                    )
+                    select distinct students_group_number from students where student_id in (select student_id from educated_students)
+                    """,
+
+                ),
+                {"field_id": field_id}
+            )
+
+            groups = []
+            for group_tuple in res:
+                groups.append(
+                    group_tuple[0]
+                )
+
+            return groups
+
+    except SQLAlchemyError as e:
+        return HTTPException(400, f"Bad DB request.\nCause:{e}")
+
+
+@router.get("/professor/get_group_comprehension")
+async def get_group_comprehension(group_name: str, field_id: str) -> List[StudentComprehension] | HTTPException:
     try:
         session: AsyncSession
         async with async_sessionmaker() as session:
             query = await session.execute(
                 text(
                     """
-                    with professor_fields as (
-                        select * from fields where professor_id = :professor_id
-                    ), educated_students as (
-                        select * from field_comprehensions where field in (select field_id from professor_fields)
-                    )
-                    select distinct students_group_number from students where student_id in (select student_id from educated_students)
+                    select f.student_id, last_name, first_name, patronymic, mark from students s join field_comprehensions f on s.student_id = f.student_id where students_group_number = :group_name and field_id = :field_id
                     """
                 ),
-                {"professor_id": professor_id}
+                {
+                    "group_name": group_name,
+                    "field_id": field_id
+                }
             )
 
             groups = []
             for group_tuple in query:
                 groups.append(
-                    group_tuple[0]
+                    StudentComprehension(
+                        student_id=int(group_tuple[0]),
+                        name=" ".join(group_tuple[1:4]),
+                        mark=int(group_tuple[4]) if group_tuple[4] not in ["", " "] else 0
+                    )
                 )
 
             return groups
